a8a8042ed48ff0207f3ac3baf55267402409eabc
Granero0011 <33851763+Granero0011@users.noreply.github.com>
2019-12-12 17:36:10 -0600

    Add files via upload

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,13 +2,48 @@
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
+    merged_arr[:len(arrA)]=arrA
+    merged_arr[len(arrA):]=arrB
+
     
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
+    if len(arr) >1: 
+        mid = len(arr)//2 #Finding the mid of the array 
+        L = arr[:mid] # Dividing the array elements  
+        R = arr[mid:] # into 2 halves 
+  
+        merge_sort(L) # Sorting the first half 
+        merge_sort(R) # Sorting the second half 
+  
+        i = j = k = 0
+          
+        # Copy data to temp arrays L[] and R[] 
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i] 
+                i+=1
+            else: 
+                arr[k] = R[j] 
+                j+=1
+            k+=1
+          
+        # Checking if any element was left 
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+=1
+            k+=1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+=1
+            k+=1
+  
+
+
     # TO-DO
 
     return arr
@@ -16,11 +51,28 @@ def merge_sort( arr ):
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
+    L = arr[start:mid]
+    R = arr[mid:end]
+    i = 0
+    j = 0
+    k = start
+    for l in range(k,end):
+        if j >= len(R) or (i < len(L) and L[i] < R[j]):
+            A[l] = L[i]
+            i = i + 1
+        else:
+            A[l] = R[j]
+            j = j + 1  
     # TO-DO
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+def merge_sort_in_place(arr, p, r):
+     if r - p > 1:
+        mid = int((p+r)/2)
+        merge_sort_in_place(arr,p,mid)
+        merge_sort_in_place(arr,mid,r)
+        merge_in_place(arr,p,mid,r)
     # TO-DO
 
     return arr
